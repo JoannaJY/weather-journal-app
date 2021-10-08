5b2ccb1e3f3a6487324fc01530d51a2718644e41
Joanna YU <joannayu@Haos-iMac.local>
2021-10-09 05:05:49 +1030

    first time HTML CSS

--- website/app.js
+++ website/app.js
@@ -1,5 +1,6 @@
 /* Global Variables */
-
+const baseUrl = 'http://api.openweathermap.org/data/2.5/weather?zip=';
+const APIKey = 'a3a01efea177cd6f42cfac7a32ae9bfa';
 // Create a new date instance dynamically with JS
 let d = new Date();
 let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
